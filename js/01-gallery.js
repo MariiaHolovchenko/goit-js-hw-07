d99c1d16aa455d8da59c9a7e987b7ed8b68be0fd
MariiaHolovchenko <mgolovchenko268@gmail.com>
2021-12-21 15:31:16 +0200

    gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const itemMarkup = galleryItems.map(galleryItem => {
+        return `<div class='gallery__item'><a class="gallery__link" href="${galleryItem.original}" ><img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}"></a></div>`
+    }).join('')
+
+gallery.insertAdjacentHTML('afterbegin', itemMarkup)
+
+gallery.addEventListener('click', onPickItem)
+function onPickItem(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return
+    }
+    
+    console.log(event.target.getAttribute(['data-source']));
+
+    const open = basicLightbox.create(`
+		<img width="1400" height="900" src="${event.target.getAttribute(['data-source'])}">
+	`);
+    open.show();
+
+    //Escape
+    window.addEventListener('keydown', onEscKeyPress);
+    function onEscKeyPress(event) {
+        if (event.code === 'Escape') {
+            open.close();
+             window.removeEventListener('keydown', onEscKeyPress)
+        }
+    }
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,15 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const itemMarkup = galleryItems.map(galleryItem => {
+        return `<a class="gallery__item" href="${galleryItem.original}"><img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" /></a>`
+    }).join('')
+gallery.insertAdjacentHTML('afterbegin', itemMarkup)
+
+
+// Библиотека
+let lightbox = new SimpleLightbox('.gallery a',{captionDelay:'250ms', captionsData:'alt'});
+lightbox.close()
